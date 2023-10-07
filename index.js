ae2d2d5b989a500058a519f950c7fb3b91943287
triguns123 <136328343+triguns123@users.noreply.github.com>
2023-10-07 13:14:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.get('./', (_, resp) => {
     resp.write("This is node malil file!");
     resp.end();
-}).listen(5400);
+}).listen(port);
